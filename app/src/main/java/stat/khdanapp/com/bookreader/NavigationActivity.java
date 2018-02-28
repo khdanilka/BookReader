61bb0f15e2c3124280d2115274b0df72f8d3179a
khdanilka <daniil.khalupskiy@gmail.com>
2018-02-28 12:25:43 +0500

    catalog_tabs added

--- app/src/main/java/stat/khdanapp/com/bookreader/NavigationActivity.java
+++ app/src/main/java/stat/khdanapp/com/bookreader/NavigationActivity.java
@@ -1,9 +1,11 @@
 package stat.khdanapp.com.bookreader;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -64,7 +66,7 @@ public class NavigationActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation, menu);
+        getMenuInflater().inflate(R.menu.navigation_3dots, menu);
         return true;
     }
 
@@ -86,17 +88,34 @@ public class NavigationActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Handle navigation_3dots view item clicks here.
+        android.support.v4.app.Fragment fragment = null;
+        Class fragmentClass = null;
         int id = item.getItemId();
-
         if (id == R.id.nav_catalog) {
-            // Handle the camera action
+            fragmentClass = Catalog_tabs.class;
         } else if (id == R.id.nav_my_book) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_settings) {
 
+        }
+
+        if (fragmentClass!= null) {
+            try {
+                fragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.frame_content, fragment).commit();
+            // Выделение существующего элемента выполнено с помощью
+            // NavigationView
+            item.setChecked(true);
+            // Установить заголовок для action bar'а
+            setTitle(item.getTitle());
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
