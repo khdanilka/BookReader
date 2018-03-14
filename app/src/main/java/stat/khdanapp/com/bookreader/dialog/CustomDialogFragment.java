ad2f3b73389d9998dadb426ccc29572d44ff9c4d
khdanilka <daniil.khalupskiy@gmail.com>
2018-03-14 15:08:12 +0500

    dialog fragment added, item can be deleted by long tap

--- app/src/main/java/stat/khdanapp/com/bookreader/dialog/CustomDialogFragment.java
+++ app/src/main/java/stat/khdanapp/com/bookreader/dialog/CustomDialogFragment.java
@@ -1,13 +1,26 @@
 package stat.khdanapp.com.bookreader.dialog;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
+import stat.khdanapp.com.bookreader.R;
+
 
 public class CustomDialogFragment extends DialogFragment {
+
+    public static final String TAG_WEIGHT_SELECTED = "weight";
+
+    public CustomDialogFragment() {
+        //
+    }
+
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -16,8 +29,16 @@ public class CustomDialogFragment extends DialogFragment {
                 .setTitle("Диалоговое окно")
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setMessage("Вы действительно хотите удалить элемент?")
-                .setPositiveButton("OK", null)
+                .setPositiveButton("Удалить", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent();
+                        //intent.putExtra(TAG_WEIGHT_SELECTED, mNpWeight.getValue());
+                        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
+                    }
+                })
                 .setNegativeButton("Отмена", null)
                 .create();
     }
+
+
 }
